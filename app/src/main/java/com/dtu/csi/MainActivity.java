bc1280d06346026260fb6ffbcd4a99db93ef4e37
Arpit Gogia <arpitgogia@protonmail.com>
2017-01-15 16:54:00 +0530

    Clean Up

--- app/src/main/java/com/dtu/csi/MainActivity.java
+++ app/src/main/java/com/dtu/csi/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
-import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
@@ -44,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_test);
+        setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         if (toolbar != null) {
             setSupportActionBar(toolbar);
--- app/src/main/java/com/dtu/csi/TestActivity.java
+++ app/src/main/java/com/dtu/csi/TestActivity.java
@@ -8,6 +8,6 @@ public class TestActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_test);
+        setContentView(R.layout.activity_main);
     }
 }
